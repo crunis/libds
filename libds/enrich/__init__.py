3a4577d66a19483606d42394b22ab06d4c77a287
Christian <christian13@gmail.com>
2024-11-04 12:14:19 +0100

    demo functions

--- libds/enrich/__init__.py
+++ libds/enrich/__init__.py
@@ -3,3 +3,4 @@ import pandas as pd
 from libds.periods import compute_periods, dates_to_ordinal
 from .lab import compute_all_penias
 from .rc import compute_rc
+from .demo import add_exitus_info, add_age
---
+++ libds/enrich/demo.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def add_exitus_info(exitus_dt, ref_dt):
+    """Compute days from reference date to exitus date and if it is within 30 or 60 days"""
+    if exitus_dt is not None:
+        days = (exitus_dt - ref_dt).days
+        exitus_30d = (days <= 30)
+        exitus_60d = (days <= 60)
+        return pd.Series(dict(
+            exitus_days = days,
+            exitus_30d = exitus_30d,
+            exitus_60d = exitus_60d
+        ))
+    return pd.Series()
+
+def add_age(birth_dt, ref_dt):
+    """Compute age in years from birth date to reference date"""
+    return pd.Series({'age': int((ref_dt - birth_dt).days/365.2425) })
